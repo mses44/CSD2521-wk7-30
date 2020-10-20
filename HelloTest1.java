c3f0540912dc05d3d974013a71f24ec47c31c89c
mses44 <51460821+mses44@users.noreply.github.com>
2020-10-20 17:31:37 -0400

    Update HelloTest1.java
    Test changes

--- HelloTest1.java
+++ HelloTest1.java
@@ -1,11 +1,12 @@
 /*
  * Created on 8/3/18 by AJT
+ * Updated on 10/20/2020 XYZ
  */
 package hello.test.pkg1;
 
 public class HelloTest1 {
     /*
-     * Replace your comments here!!
+     * This is my first pull request
      */
     public static void main(String[] args) {
         System.out.println("Welcome");
